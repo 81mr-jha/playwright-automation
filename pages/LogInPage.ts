08a969c02a5c12d01f32585e2216a3987f0b569e
81mr-jha <ashutoshchandra1523@gmail.com>
2025-07-23 04:30:25 +0530

    Sixth commit

--- pages/LogInPage.ts
+++ pages/LogInPage.ts
@@ -1,10 +1,9 @@
-import {Locator,Page} from '@playwright/test';
+import { Locator, Page, expect } from '@playwright/test';
 
 export class LogInPage {
-    readonly page : Page;
+    readonly page: Page;
 
-    constructor(page : Page)
-    {
+    constructor(page: Page) {
         this.page = page;
         // this.EnterEmail = page.locator('//input[@data-qa="login-email"]')
     }
@@ -13,13 +12,77 @@ export class LogInPage {
         await this.page.locator('//div[@class="login-form"]/h2');
     }
 
-    async enterNameAndEmail(email : string, password : string)
-    {
-        await this.page.fill('input[data-qa="login-email"]',email);
-        await this.page.fill('input[data-qa="login-password"]',password);
+    async enterNameAndEmail(email: string, password: string) {
+        await this.page.fill('input[data-qa="login-email"]', email);
+        await this.page.fill('input[data-qa="login-password"]', password);
     }
 
-    async clickOnLogInButton(){
-        await this.page.click('')
+    async clickOnLogInButton() {
+        await this.page.click('button[data-qa="login-button"]');
     }
+
+    async verifyHomePage() {
+        await expect(this.page).toHaveURL('https://automationexercise.com/');
+    }
+
+    async verifyErrorIsVisible() {
+        await expect(this.page.locator("//p[text()='Your email or password is incorrect!']")).toBeVisible();
+    }
+
+    async verifyLoggedInAsUsername() {
+        await expect(this.page.locator('//a[text()=" Logged in as "]')).toBeVisible();
+    }
+
+    async clickOnLogout() {
+        // await this.page.locator('a:has-text("Logout")').click();
+        await this.page.getByRole('link', { name: 'Logout' }).click();
+
+    }
+
+    async verifyLoginPage() 
+    {
+        await expect(this.page).toHaveURL('https://automationexercise.com/login');
+    }
+
+    async verifyTestCasesOnHomePage() 
+    {
+        await expect(this.page.locator('//a[text()=" Test Cases"]')).toBeVisible();
+    }
+
+    async clickonTestCaseButton() 
+    {
+        await this.page.locator('//a[text()=" Test Cases"]').click();
+    }
+
+    async verifyTestCasePage() 
+    {
+        await expect(this.page).toHaveURL('https://automationexercise.com/test_cases');
+    }
+
+    async clickOnProducts() 
+    {
+        await this.page.locator('//a[text()=" Products"]').click();
+    }
+
+    async verifyAllProductsPage() 
+    {
+        await expect(this.page).toHaveURL('https://automationexercise.com/products');
+    }
+
+    async verifyProductList() 
+    {
+        await expect(this.page.getByText('All Products')).toBeVisible();
+    }
+
+    async clickOnViewProduct() 
+    {
+        await this.page.locator('//div[@class="features_items"]/div[2]/div/div[2]/ul/li/a').click();
+    }
+
+    async verifyProductDetailPage()
+    {
+        await expect(this.page).toHaveURL('https://automationexercise.com/product_details/1');
+    }
+
+    
 }
--- tests/Automation-Excercise/loginuser.spec.ts
+++ tests/Automation-Excercise/loginuser.spec.ts
@@ -1,8 +1,8 @@
-import {test} from '@playwright/test';
+import { test } from '@playwright/test';
 import { HomePage } from '../../pages/HomePage';
 import { LogInPage } from '../../pages/LogInPage';
 
-test('Login User - Automation Excercise', async ({page}) =>{
+test('Login User - Automation Excercise', async ({ page }) => {
     const homePage = new HomePage(page);
     const loginPage = new LogInPage(page);
 
@@ -11,6 +11,76 @@ test('Login User - Automation Excercise', async ({page}) =>{
     await homePage.clickSignupLogin();
 
     await loginPage.verifyLogInToYourAccountIsVisible();
-    await loginPage.enterNameAndEmail('bahubali@gmail.com','P@ssw0rd');
+    await loginPage.enterNameAndEmail('bahubali@gmail.com', 'P@ssw0rd');
     await loginPage.clickOnLogInButton();
+
+    await loginPage.verifyHomePage();
+})
+
+test('Incorrect email & password', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LogInPage(page);
+
+    await homePage.goto();
+    await homePage.verifyHomePageVisible();
+    await homePage.clickSignupLogin();
+
+    await loginPage.verifyLogInToYourAccountIsVisible();
+    await loginPage.enterNameAndEmail("ashutosh@gmail.com", 'P@ssw0rd');
+    await loginPage.clickOnLogInButton();
+    await loginPage.verifyErrorIsVisible();
+})
+
+test('Logout User', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LogInPage(page);
+
+    await homePage.goto();
+    await homePage.verifyHomePageVisible();
+    await homePage.clickSignupLogin();
+
+    await loginPage.verifyLogInToYourAccountIsVisible();
+    await loginPage.enterNameAndEmail('bahubali@gmail.com', 'P@ssw0rd');
+    await loginPage.clickOnLogInButton();
+    await loginPage.verifyLoggedInAsUsername();
+    await loginPage.clickOnLogout();
+    await loginPage.verifyLoginPage();
+})
+
+test('Verify Test Cases Page', async ({page}) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LogInPage(page);
+
+    await homePage.goto();
+    await homePage.verifyHomePageVisible();
+
+    await loginPage.clickonTestCaseButton();
+    await loginPage.verifyTestCasePage();
+})
+
+test('Verify All Products and product detail page', async ({page}) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LogInPage(page);
+
+    await homePage.goto();
+    await homePage.verifyHomePageVisible();
+
+    await loginPage.clickOnProducts();
+    await loginPage.verifyAllProductsPage();
+    await loginPage.verifyProductList();
+    await loginPage.clickOnViewProduct();
+    await loginPage.verifyProductDetailPage();
+})
+
+test('Search Product', async ({page}) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LogInPage(page);
+
+    await homePage.goto();
+    await homePage.verifyHomePageVisible();
+
+    await loginPage.clickOnProducts();
+    await loginPage.verifyAllProductsPage();
+
+    // await loginPage.
 })
