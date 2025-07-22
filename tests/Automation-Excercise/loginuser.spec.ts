6417a1b0f13da9cd0f77650a767f3dd07bd45f64
81mr-jha <ashutoshchandra1523@gmail.com>
2025-07-22 20:05:11 +0530

    fifth commit 1

--- tests/Automation-Excercise/loginuser.spec.ts
+++ tests/Automation-Excercise/loginuser.spec.ts
@@ -1,7 +1,16 @@
 import {test} from '@playwright/test';
 import { HomePage } from '../../pages/HomePage';
+import { LogInPage } from '../../pages/LogInPage';
 
 test('Login User - Automation Excercise', async ({page}) =>{
     const homePage = new HomePage(page);
-    
+    const loginPage = new LogInPage(page);
+
+    await homePage.goto();
+    await homePage.verifyHomePageVisible();
+    await homePage.clickSignupLogin();
+
+    await loginPage.verifyLogInToYourAccountIsVisible();
+    await loginPage.enterNameAndEmail('bahubali@gmail.com','P@ssw0rd');
+    await loginPage.clickOnLogInButton();
 })
